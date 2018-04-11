caabb6c9b453a0245b41471450bee20d78fc0714
Heisler.Luke <Heisler.Luke@SEMS-259-37.SEMS259.LAB>
2018-04-11 14:58:04 -0700

    prime or not

---
+++ src/PrimeOrNot.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+
+public class PrimeOrNot {
+
+	public static void main(String[] args) {
+		boolean prime = true;
+		String number = JOptionPane.showInputDialog("What number do you want to check for primeness?");
+		int n = Integer.parseInt(number);
+		for(int i = 1; i < n+1; i++) {
+			if(n%i==n/2) {
+				JOptionPane.showMessageDialog(null, "It's not a prime number...");
+				prime = false;
+				break;
+			}
+		}
+		if(prime==true) {
+			JOptionPane.showMessageDialog(null, "The number is prime!");
+		}
+	}
+}
+
